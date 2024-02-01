d04671b2606c0f067a5c368c34be0153db88931c
BuildTools <unconfigured@null.spigotmc.org>
2024-02-01 16:43:12 +0100

    finition du JS du formulaire de contacts

--- src/Pages/Nous contacter.jsx
+++ src/Pages/Nous contacter.jsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
 import "../css/Nous contacter.css"
 import {useForm} from "react-hook-form";
+import emailjs from "@emailjs/browser"
 
 const NousContacter = () => {
     const {
@@ -10,38 +10,70 @@ const NousContacter = () => {
         formState: { errors },
     } = useForm()
 
+    const onSubmitForm = (data) => {
+        emailjs.send(
+            'service_l4b05zp',
+            'template_y8y10ur',
+            {
+                subject: data.subject,
+                email: data.email,
+                message: data.message,
+                name: data.name
+            },
+            'RkW-jZKy0IcqEfXxt'
+        )
+    }
+
     return (
         <main className="contacts">
-            <form>
+            <form onSubmit={handleSubmit(onSubmitForm)}>
                 <label htmlFor="name">Nom</label><br/>
                 <input {...register("name")} type="text" name="name" placeholder="Votre nom"/><br/>
 
                 <label htmlFor="mail">Mail</label><br/>
-                <input {...register("mail")} type="email" placeholder="Adresse mail" name="mail"
-                       onBlur={(e) =>checkMail(e.target.value)} required/><br/><br/>
+                <input {...register("email", {required: true})} type="mail" name="email" placeholder="Adresse mail"/><br/>
+                <br/>
 
                 <label htmlFor="subject">Sujet</label><br/>
-                <input {...register("subject")} type="text" name="subject" placeholder="Sujet de votre demande" required/><br/><br/>
+                <input {...register("subject")} type="text" name="subject" placeholder="Sujet de votre demande"/><br/><br/>
 
                 <label htmlFor="message">Message</label><br/>
                 <textarea {...register("message")} name="message" placeholder="Votre message" style={{
                     resize: "none",
                     width: "100%"
                 }} rows={10}></textarea><br/>
-
+                {errors.email && <p style={{
+                    color: "red"
+                }}>Email recquis</p>}
                 <input type="submit" value="Envoyer"/>
-
-
             </form>
         </main>
-    );
-    function checkMail(mail) {
-        if (!mail.includes('@')) {
-            alert("Il n'y a pas d'@, cette adresse mail n'est pas valide !")
-        } else {
-            setValid(true)
-        }
-    }
+    )
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     watch,
+    //     formState: { errors },
+    // } = useForm()
+    //
+    // const onSubmit = (data) => console.log(data)
+    //
+    // console.log(watch("example")) // watch input value by passing the name of it
+    //
+    // return (
+    //     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    //     <form onSubmit={handleSubmit(onSubmit)}>
+    //         {/* register your input into the hook by invoking the "register" function */}
+    //         <input defaultValue="test" {...register("example")} />
+    //
+    //         {/* include validation with required or other standard HTML validation rules */}
+    //         <input {...register("exampleRequired", { required: true })} />
+    //         {/* errors will return when field validation fails  */}
+    //         {errors.exampleRequired && <span>This field is required</span>}
+    //
+    //         <input type="submit" />
+    //     </form>
+    // )
 };
 
 
