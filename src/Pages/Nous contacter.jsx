273922069c274a9b96ddd2209adbffabe1641b63
BuildTools <unconfigured@null.spigotmc.org>
2024-02-01 14:11:29 +0100

    Mise à jour du projet

--- src/Pages/Nous contacter.jsx
+++ src/Pages/Nous contacter.jsx
@@ -1,22 +1,42 @@
 import React, {useState} from 'react';
+import "../css/Nous contacter.css"
+import {useForm} from "react-hook-form";
 
 const NousContacter = () => {
     const [valid, setValid] = useState(false)
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
     return (
-        <main>
+        <main className="contacts">
             <form>
-                <input type="text" placeholder="Nom"/><br/>
-                <input type="email" placeholder="Adresse mail" onBlur={(e) =>checkMail(e.target.value)} required/><br/>
-                <textarea placeholder="Votre message" style={{
+                <label htmlFor="name">Nom</label><br/>
+                <input {...register("name")} type="text" name="name" placeholder="Votre nom"/><br/>
+
+                <label htmlFor="mail">Mail</label><br/>
+                <input {...register("mail")} type="email" placeholder="Adresse mail" name="mail"
+                       onBlur={(e) =>checkMail(e.target.value)} required/><br/><br/>
+
+                <label htmlFor="subject">Sujet</label><br/>
+                <input {...register("subject")} type="text" name="subject" placeholder="Sujet de votre demande" required/><br/><br/>
+
+                <label htmlFor="message">Message</label><br/>
+                <textarea {...register("message")} name="message" placeholder="Votre message" style={{
                     resize: "none",
-                    width: 200,
-                    height: 200
-                }}></textarea><br/>
+                    width: "100%"
+                }} rows={10}></textarea><br/>
+
                 <input type="submit" value="Envoyer" onClick={(e) => {
                     if (!valid) {
                         e.preventDefault()
                     }
                 }}/>
+
+
             </form>
         </main>
     );
