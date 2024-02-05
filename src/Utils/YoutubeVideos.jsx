cc3d8488a612e2030b6ec14ba147135eb8e90b71
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 09:06:17 +0100

    Rajout d'un pseudo-module pour les vidéos

---
+++ src/Utils/YoutubeVideos.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const YoutubeVideos = props => {
+    // noinspection XmlDeprecatedElement
+    return (
+        <div>
+            <iframe width="921" height="518" src={"https://www.youtube.com/embed/" + props.id} title={props.name}
+                    frameBorder={0}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen></iframe>
+        </div>
+    );
+};
+
+YoutubeVideos.propTypes = {
+    id: PropTypes.string,
+    name: PropTypes.string
+};
+
+export default YoutubeVideos;
